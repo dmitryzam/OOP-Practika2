5a2b9b40d0b8c0aa9d03345f732864d1de119e93
dmitryzam <dmitryzam@mail.ru>
2024-08-16 08:33:43 +0500

    Add project files.

---
+++ OOP-Practika2/Main.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<string>
+#include"Map.hpp"
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	std::cout << "Создадим первую коллекцию:\n";
+	Map<std::string, int> mp1;
+	std::cout << "Размер коллекции 1 - " << mp1.size() << " до добавления элементов.\n";
+
+	mp1.setItem("Russia", 7);
+	mp1.setItem("USA", 1);
+	mp1.setItem("France", 33);
+	mp1.setItem("Japan", 81);
+	mp1.setItem("Japan", 81);
+	std::cout << "Размер коллекции 1 - " << mp1.size() << " после добавления элементов.\n";
+			
+	std::cout << "Телефонный код Japan - +" << mp1["Japan"] << '\n';
+	std::cout << "Телефонный код France - +" << mp1["France"] << '\n';
+	std::cout << "Телефонный код Russia - +" << mp1["Russia"] << '\n';
+
+	std::cout << "\nСоздадим новую коллекцию на основе первой коллекции:\n";
+	Map<std::string, int> mp2(mp1);
+	std::cout << "Размер коллекции 2 - " << mp2.size() << '\n';
+	std::cout << "Телефонный код Japan - +" << mp2["Japan"] << '\n';
+	std::cout << "Телефонный код France - +" << mp2["France"] << '\n';
+	std::cout << "Телефонный код Russia - +" << mp2["Russia"] << '\n';
+
+	//std::cout << "\nСоздадим третью коллекцию с помощью списка инициализации:\n";
+
+	//Map<std::string, int> mp3(("India", 91), ("Belarussia", 375), ("Avstralia", 61));
+	//Map<std::string, int> mp3("India", 91);
+
+
+
+	
+	
+
+
+
+
+	return 0;
+}
---
+++ OOP-Practika2/Map.hpp
@@ -0,0 +1,96 @@
+﻿#ifndef _MAP_HPP
+#define _MAP_HPP
+#include<iostream>
+#include<exception>
+#include<initializer_list>
+
+template<typename ANY1, typename ANY2>
+class Map {
+private:
+	struct Node {
+		ANY1 m_key = ANY1();
+		ANY2 m_value = ANY2();
+	};
+	Node* node;
+	size_t length;
+public:
+	Map() : node(nullptr), length(0) {}
+	Map(const Map& other) : length(other.length) {
+		if (length == 0) {
+			node = nullptr;			
+			return;
+		}
+		node = new Node[length]{};
+		for (size_t i{}; i < length; ++i) {
+			node[i] = other.node[i];			
+		}		
+	}
+	/*
+	Map(size_t size) : length(size) {
+		node = new Node[length];
+	}
+	Map(const std::initializer_list<ANY1, ANY2>& init) : Map(init.size()){
+		for (size_t i{}; i < length; ++i)
+			node[i] = init[i];
+	}*/
+	~Map() {
+		delete[] node;
+	}
+	// метод, кот. принимает ключ и значение, и добавляет их в новый элемент,
+	// также осущ. проверку от повторение ключей:
+	void setItem(const ANY1 key, const ANY2 value) {
+		if (length == 0) {
+			//std::cout << "Prog1\n";
+
+			Node* new_node = new Node[1];
+			new_node[0].m_key = key;
+			new_node[0].m_value = value;
+			node = new_node;
+			++length;
+			return;
+		}
+		// Добавить проверку
+		for (size_t i{}; i < length; ++i) {
+			if (key == node[i].m_key) {
+				//std::cout << "Prog2\n";
+				return;
+			}
+		}
+		//std::cout << "Prog3\n";
+		Node* new_node = new Node[length + 1];
+		for (size_t i{}; i < length; ++i) {
+			new_node[i] = node[i];
+		}
+		delete[] node;
+		node = new_node;
+		node[length].m_key = key;
+		node[length].m_value = value;
+		++length;
+	}
+	// метод возвращает ссылку на значение, переданного ключа:
+	const ANY2& operator[](const ANY1& key)const {
+		if (length == 0)
+			return ANY2();	// ANY2()		
+		for (size_t i{}; i < size(); ++i) {
+			if (key == node[i].m_key) {
+				//std::cout << "Prog4\n";
+				return node[i].m_value;
+			}			
+		}
+		//std::cout << "Prog5\n";
+		return ANY2();	// ANY2()
+	}
+	// метод, возвращающий размер коллекции:
+	size_t size()const {
+		return length;
+	}
+	
+	// метод очищает коллекцию:
+	void clear() {
+		delete[] node;
+		node = nullptr;
+		length = 0;
+	}
+};
+
+#endif // _MAP_HPP
